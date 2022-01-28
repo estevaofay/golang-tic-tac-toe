5935bd69f45336703afdd6fee03061ca4e063265
Rafael Leandro Araujo <rafaraujo@ciandt.com>
2022-01-28 10:06:06 -0300

    REad input

--- src/player/player.go
+++ src/player/player.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 var reader = bufio.NewReader(os.Stdin)
@@ -18,8 +20,17 @@ func (p *Player) GetPlayerMove() int {
 	fmt.Printf("%s what is your next move? (1-9):\n", p.Name)
 
 	// Get player Move from Stdin
+	text, _ := reader.ReadString('\n')
+	text = strings.ReplaceAll(text, "\n", "")
+
 	// Check if move is valid
+	userMoveInt, _ := strconv.Atoi(text)
+
 	// userMoveInt := 0
+	if userMoveInt < 1 || userMoveInt > 9 {
+		fmt.Println("Please insert a number between 1 and 9")
+	}
+
 }
 
 func (p *Player) PrintPlayerScore() {
