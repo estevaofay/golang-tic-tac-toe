e544c4048eab33104dbf2558d37bd3b0aeb376b1
Silvio Silva <smsilva@gmail.com>
2022-01-28 09:44:44 -0300

    ficou uma merda mas fui eu que fiz

--- src/player/player.go
+++ src/player/player.go
@@ -2,17 +2,26 @@ package player
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 )
 
 var reader = bufio.NewReader(os.Stdin)
 
 type Player struct {
-	// Define Player struct here
+	Name  string
+	Score int
+	Mark  rune
 }
 
 func (p *Player) GetPlayerMove() int {
+	fmt.Printf("%s what is your next move? (1-9):\n", p.Name)
+
 	// Get player Move from Stdin
 	// Check if move is valid
 	// userMoveInt := 0
+}
+
+func (p *Player) PrintPlayerScore() {
+	fmt.Printf("Player %s has as score of %d\n", p.Name, p.Score)
 }
